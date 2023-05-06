6f36d22bc50722fef39510ce18e3517b7757f566
vnhk <47853604+vnhk@users.noreply.github.com>
2023-05-06 17:41:14 +0200

    Login errors dialogs

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -2,6 +2,10 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthService} from "../session/auth.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {catchError} from "rxjs/operators";
+import {HttpErrorResponse} from "@angular/common/http";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -14,7 +18,8 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder,
               private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              public snackBar: MatSnackBar) {
 
     this.form = this.fb.group({
       username: ['', Validators.required],
@@ -27,13 +32,35 @@ export class LoginComponent {
 
     if (val.username && val.password) {
       this.authService.login(val.username, val.password)
-        .subscribe(
-          value => {
-            this.authService.setSession(value);
-            this.router.navigateByUrl('/');
-          }
-        );
+        .pipe(
+          catchError(this.handleError.bind(this))
+        ).subscribe(
+        value => {
+          this.authService.setSession(value);
+          this.router.navigateByUrl('/');
+        }
+      );
     }
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if(error.status === 401) {
+      this.showErrorPopup(error.error.message);
+    } else {
+      this.showErrorPopup("Cannot login. Contact with administrator.");
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+  private showErrorPopup(message: string) {
+    this.openBarWithMessage(message, ['error-bar'], 15000);
+  }
+
+  private openBarWithMessage(message: string, classes: string[], duration: number) {
+    this.snackBar.open(message, "Ok", {
+      duration: duration,
+      panelClass: classes
+    });
   }
 
 }
